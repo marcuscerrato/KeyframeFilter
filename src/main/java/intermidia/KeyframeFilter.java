ab9325dcfa40e19e1fea8de82fd6fc7ce0c0701a
marcuscerrato <marcus.cerrato@gmail.com>
2020-05-18 16:26:48 -0300

    Add files

---
+++ src/main/java/intermidia/KeyframeFilter.java
@@ -0,0 +1,58 @@
+package intermidia;
+
+import java.io.File;
+
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.video.processing.shotdetector.VideoKeyframe;
+
+import TVSSUnits.Shot;
+import TVSSUnits.ShotList;
+import TVSSUtils.KeyframeReader;
+
+public class KeyframeFilter
+{
+	/*Usage: <in: keyframe folder> <out: filtered keyframe folder>*/
+    public static void main( String[] args ) throws Exception
+    {
+    	String inputFolder = args[0];
+    	String outputFolder = args[1];
+    	
+    	
+    	/*
+    	VFSListDataset<MBFImage> keyframes = new VFSListDataset<MBFImage>(
+    			inputFolder, ImageUtilities.MBFIMAGE_READER);
+
+    	int conta = 0;
+    	for (MBFImage keyframe: keyframes) {
+    		MBFImage filteredImage = keyframe.processInplace(new FGaussianConvolve(2f));
+    		String number = String.format("%09d", conta);
+    		String outputName = "filteredKeyframe" + number + ".jpg";
+    		ImageUtilities.write(filteredImage, new File(outputFolder + outputName));
+    		conta++;
+    	}
+    	*/
+
+    	
+    	
+    	ShotList shotList = KeyframeReader.readFromFolder(inputFolder);
+    		
+    	int shotNum = 0;
+    	for(Shot shot: shotList.getList())
+    	{
+    		int keyframeNum = 0;
+    		for(VideoKeyframe<MBFImage> keyframe: shot.getKeyFrameList())
+    		{ 
+        		MBFImage filteredImage = keyframe.imageAtBoundary.processInplace(
+        				new FGaussianConvolve(2f));
+        		String keyframeNumber = String.format("%04d", keyframeNum);
+        		String shotNumber = String.format("%04d", shotNum);
+        		String outputName = "s" + shotNumber + "kf" + keyframeNumber + ".jpg";
+        		ImageUtilities.write(filteredImage, new File(outputFolder + outputName));
+        		keyframeNum++;
+    		}
+    		shotNum++;
+    	}
+    }
+}
